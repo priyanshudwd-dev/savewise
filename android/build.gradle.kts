8848efebc4ab3ca6f71b530124c776b4f40f0b76
priyanshudwd-dev <priyanshudwd@gmail.com>
2026-08-27 16:19:43 +0530

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -32,3 +32,11 @@ subprojects {
         }
     }
 }
+subprojects {
+    afterEvaluate {
+        if (project.hasProperty("android")) {
+            val android = project.extensions.findByName("android") as? com.android.build.gradle.BaseExtension
+            android?.compileSdkVersion(36)
+        }
+    }
+}
